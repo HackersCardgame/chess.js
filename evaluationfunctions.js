7311910e2a08957cf8ad5fb8af3fb6b2ec05f04d
Marc jr Landolt <mail@marclandolt.ch>
2018-07-23 04:37:16 +0200

    pawn all checks implemented

--- evaluationfunctions.js
+++ evaluationfunctions.js
@@ -7,23 +7,25 @@ function check(from, to) {
 
 
 function checkPawn(from, to) {
-  if (document.getElementById(from).innerHTML=="♟") {
+  player = 0;
+  if (document.getElementById(from).innerHTML=="♟") 
+    player = 1;
+  if (document.getElementById(from).innerHTML=="♙") 
+    player = -1;
+  
+  if(player == 0) return false;
 
     //check single move
     delta = sub(getXY(to), getXY(from));
-    if(comp(delta, [0, 1])) return true;
+    if(comp(delta, [0, 1*player]) && !isEnemy(from,to)) return true;
 
     //check double start move
-    if(comp(delta, [0, 2]) && getXY(from)[1]==1) return true;
+    if(comp(delta, [0, 2*player]) && (getXY(from)[1]==1 ||getXY(from)[1]==6)) return true;
     
-    return false;
-  }
+    //check diagonal 1 when attacking enemy
+    if( ( comp(delta, [1*player, 1*player]) && isEnemy(from, to) ) || ( comp(delta, [-1*player, 1*player]) && isEnemy(from, to) ) ) return true;
 
-  if (document.getElementById(from).innerHTML=="♙") {
-    delta = sub(getXY(to), getXY(from));
-    if(comp(delta, [0, -1])) return true;
-    else return false;
-  }
+    return false;
 
 }
 
@@ -31,4 +33,18 @@ function getXY(place) {
   y = place.substring(1,2);
   x = place.substring(2,3);
   return [x, y]
+}
+
+function isEnemy(from, to) {
+  me = document.getElementById(from).innerHTML;
+  he = document.getElementById(to).innerHTML;
+  if(me == "♕" || me == "♔" || me == "♗" || me == "♘" || me == "♖" || me == "♙")
+    if(he == "♛" || he == "♚" || he == "♝" || he == "♞" || he == "♜" || he == "♟")
+      return true;
+
+  if(me == "♛" || me == "♚" || me == "♝" || me == "♞" || me == "♜" || me == "♟")
+    if(he == "♕" || he == "♔" || he == "♗" || he == "♘" || he == "♖" || he == "♙")
+      return true;
+      
+  return false;      
 }
